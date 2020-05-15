937bc28923abca09be1b1d9f4cfebe0d320fa2af
stefanBusiness <stefanstojanovicbusiness73@gmail.com>
2020-05-15 16:27:55 +0200

    init new

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 SECRET_KEY='W1iQb20_wiRQc-qUMoJX2A'
 STATIC_FOLDER='static/'
 
-# COMTRADE #
-COMTRADE_USERNAME ='get'
-COMTRADE_PASSWORD='get2019'
+# COMTRADE # #these probablly wont be working now
+COMTRADE_USERNAME =''
+COMTRADE_PASSWORD=''
 
-# BIG COMMERCE API #
+# BIG COMMERCE API # # #these probablly wont be working now
 CLIENT_ID='qf4tt6oqvhgkp1rkp0vg00qsmd6v3b4'
 TOKEN_ID='rrt5x16bexljp02k0emvw0az9lit7a5'
 STORE_HASH='ecvq1tuanb'
